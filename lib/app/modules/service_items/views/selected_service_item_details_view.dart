58cb958abc2bd7b7e73815f747856f346bd64f13
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-10-14 16:17:46 +0600

    overview page on service item details view UI complete

--- lib/app/modules/service_items/views/selected_service_item_details_view.dart
+++ lib/app/modules/service_items/views/selected_service_item_details_view.dart
@@ -3,9 +3,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:nuforce/app/modules/service_items/controllers/service_items_controller.dart';
 import 'package:nuforce/app/modules/service_items/widgets/little_text_with_colored_bg.dart';
+import 'package:nuforce/app/modules/service_items/widgets/rating_tile.dart';
 import 'package:nuforce/app/modules/service_items/widgets/selected_service_details_tab.dart';
 import 'package:nuforce/app/modules/service_items/widgets/total_reviwe_card.dart';
 import 'package:nuforce/app/shared/widgets/custom_appbar_minimal.dart';
+import 'package:nuforce/app/shared/widgets/primary_button.dart';
+import 'package:nuforce/app/shared/widgets/secondary_button.dart';
+import 'package:nuforce/app/utils/app_sizes.dart';
 import 'package:nuforce/app/utils/colors.dart';
 import 'package:nuforce/main.dart';
 
@@ -30,242 +34,231 @@ class _SelectedServiceItemDetailsViewState extends State<SelectedServiceItemDeta
       body: SizedBox(
         height: height,
         width: width,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SelectedServiceDetailsTabs(controller: controller),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 170.sp,
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryBlue3,
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Row(
-                      children: [
-                        const LittleTextWithColoredBG(
-                          text: '0',
-                          color: AppColors.primaryBlue2,
-                        ),
-                        SizedBox(width: 4.sp),
-                        const LittleTextWithColoredBG(
-                          text: 'Service',
-                          color: AppColors.primaryBlue1,
-                        ),
-                        SizedBox(width: 4.sp),
-                        const LittleTextWithColoredBG(
-                          text: 'IBO4',
-                          color: AppColors.primaryBlue2,
-                        ),
-                        const Spacer(),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _isFavorite = !_isFavorite;
-                            });
-                          },
-                          child: Icon(
-                            _isFavorite ? Icons.favorite : Icons.favorite_border,
-                            color: AppColors.primaryBlue1,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Stat from',
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                          'Quantity',
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '\$25.00',
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          '1',
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Category',
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                          'Taxable',
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Lawn Services',
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                          'No',
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            color: AppColors.nutralBlack1,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 16),
-                    const Divider(color: AppColors.inactiveColor),
-                    const SizedBox(height: 16),
-                    Text(
-                      'What’s Included?',
-                      style: TextStyle(
-                        color: AppColors.nutralBlack1,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      ' • Only Service Charge\n • 7 Days service warranty',
-                      style: TextStyle(
-                        color: AppColors.nutralBlack2,
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Why Choose Us?',
-                      style: TextStyle(
-                        color: AppColors.nutralBlack1,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      'Your happiness is our goal. Lorem Ipsum is sim ply dummy text of the printing and typese tting industry. Lorem Ipsum has .',
-                      style: TextStyle(
-                        color: AppColors.nutralBlack2,
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Customer Reviews',
-                      style: TextStyle(
-                        color: AppColors.nutralBlack1,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    const TotalReviewCard(),
-                    Text(
-                      'Total 22 reviews',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: AppColors.nutralBlack1,
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    ListView.builder(
-                      itemCount: 1,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(color: AppColors.inactiveColor),
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    decoration: const BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: AppColors.primaryBlue1,
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        'AK',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16.sp,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SelectedServiceDetailsTabs(controller: controller),
+            Expanded(child: overviewBody()),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: AppSizes.horizontalPadding),
+              child: Row(
+                children: [
+                  Expanded(child: SecondaryButton(onPressed: () {}, text: 'Delete')),
+                  const SizedBox(width: 15),
+                  Expanded(child: PrimaryButton(onPressed: () {}, text: 'Edit')),
+                ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget overviewBody() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 170.sp,
+              decoration: BoxDecoration(
+                color: AppColors.primaryBlue3,
+                borderRadius: BorderRadius.circular(4),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                const LittleTextWithColoredBG(
+                  text: '0',
+                  color: AppColors.primaryBlue2,
+                ),
+                SizedBox(width: 4.sp),
+                const LittleTextWithColoredBG(
+                  text: 'Service',
+                  color: AppColors.primaryBlue1,
+                ),
+                SizedBox(width: 4.sp),
+                const LittleTextWithColoredBG(
+                  text: 'IBO4',
+                  color: AppColors.primaryBlue2,
+                ),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isFavorite = !_isFavorite;
+                    });
+                  },
+                  child: Icon(
+                    _isFavorite ? Icons.favorite : Icons.favorite_border,
+                    color: AppColors.primaryBlue1,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Stat from',
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Quantity',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '\$25.00',
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '1',
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Category',
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Taxable',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Lawn Services',
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'No',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: AppColors.nutralBlack1,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Divider(color: AppColors.inactiveColor),
+            const SizedBox(height: 16),
+            Text(
+              'What’s Included?',
+              style: TextStyle(
+                color: AppColors.nutralBlack1,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              ' • Only Service Charge\n • 7 Days service warranty',
+              style: TextStyle(
+                color: AppColors.nutralBlack2,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Why Choose Us?',
+              style: TextStyle(
+                color: AppColors.nutralBlack1,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              'Your happiness is our goal. Lorem Ipsum is sim ply dummy text of the printing and typese tting industry. Lorem Ipsum has .',
+              style: TextStyle(
+                color: AppColors.nutralBlack2,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Customer Reviews',
+              style: TextStyle(
+                color: AppColors.nutralBlack1,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 16),
+            const TotalReviewCard(),
+            Text(
+              'Total 22 reviews',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: AppColors.nutralBlack1,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 16),
+            ListView.builder(
+              itemCount: 10,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                return const Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: RatingTile(),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/app/modules/service_items/widgets/rating_tile.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nuforce/app/utils/colors.dart';
+
+class RatingTile extends StatelessWidget {
+  const RatingTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: AppColors.inactiveColor),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.primaryBlue1,
+                ),
+                child: Center(
+                  child: Text(
+                    'AK',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Abdul Korim',
+                          style: TextStyle(
+                            color: AppColors.nutralBlack1,
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const Spacer(),
+                        Text(
+                          '5, June 2023',
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            color: AppColors.white4,
+                            fontSize: 11.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    RatingBar.builder(
+                      initialRating: 4,
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 14.sp,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star_rounded,
+                        color: AppColors.yellow,
+                      ),
+                      onRatingUpdate: (rating) {},
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Nibh nibh quis dolor in. Etiam cras nisi, turpis quisque diam',
+            style: TextStyle(
+              color: AppColors.nutralBlack1,
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w400,
+              letterSpacing: -0.15,
+            ),
+          ),
+          FittedBox(
+            child: Container(
+              margin: const EdgeInsets.only(top: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: AppColors.lightBlue,
+              ),
+              child: Center(
+                child: Text(
+                  'Approve',
+                  style: TextStyle(
+                    color: AppColors.primaryBlue1,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.15,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
