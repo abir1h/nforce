c3be4daae203deb6845d0e1e29f3e9be132ed33d
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-11-10 15:42:13 +0600

    subscription tile

--- lib/app/modules/service_items/widgets/subscription_items_widget.dart
+++ lib/app/modules/service_items/widgets/subscription_items_widget.dart
@@ -1,25 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:nuforce/app/modules/service_items/widgets/item_count_and_sort_widget.dart';
-import 'package:nuforce/app/modules/service_items/widgets/search_widget.dart';
+import 'package:nuforce/app/modules/service_items/widgets/subscription_list_tile.dart';
+import 'package:nuforce/app/shared/widgets/custom_dropdown.dart';
 import 'package:nuforce/app/utils/colors.dart';
 
-class SubscriptionItems extends StatelessWidget {
+class SubscriptionItems extends StatefulWidget {
   const SubscriptionItems({super.key});
 
   @override
+  State<SubscriptionItems> createState() => _SubscriptionItemsState();
+}
+
+class _SubscriptionItemsState extends State<SubscriptionItems> {
+  String? categoryDropdownValue;
+  List<String> categoryDropdownItems = ['Category 1', 'Category 2', 'Category 3'];
+  @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const SearchWidget(),
-        SizedBox(height: 20.sp),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.sp),
-          child: const ItemCountAndSort(),
+        CustomDropdownButton(
+          items: categoryDropdownItems.map((e) {
+            return DropdownMenuItem(
+              value: e,
+              child: Text(
+                e,
+                style: TextStyle(
+                  color: AppColors.subText,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            );
+          }).toList(),
+          hint: 'Select Category',
+          onChanged: (v) {
+            setState(() {
+              categoryDropdownValue = v as String?;
+            });
+          },
+          value: categoryDropdownValue,
         ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Divider(color: AppColors.greyText),
+        SizedBox(height: 14.h),
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.textFieldBackground,
+            borderRadius: BorderRadius.circular(10.sp),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Find by Name...',
+              hintStyle: TextStyle(
+                color: AppColors.subText,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.w400,
+              ),
+              border: InputBorder.none,
+              prefixIcon: const Icon(
+                Icons.search,
+                color: AppColors.subText,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 24.h),
+        Text(
+          'Subscription list',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: AppColors.nutralBlack1,
+            fontSize: 18.sp,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        SizedBox(height: 16.h),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 20,
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: EdgeInsets.only(bottom: 16.sp),
+                child: const SubscriptionListTile(),
+              );
+            },
+          ),
         ),
       ],
     );
---
+++ lib/app/modules/service_items/widgets/subscription_list_tile.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nuforce/app/modules/service_items/widgets/little_text_with_colored_bg.dart';
+import 'package:nuforce/app/utils/colors.dart';
+
+class SubscriptionListTile extends StatelessWidget {
+  const SubscriptionListTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: AppColors.white3),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Interiorscaping Experts',
+                style: TextStyle(
+                  color: AppColors.nutralBlack1,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const Icon(
+                Icons.favorite_outline,
+                color: AppColors.nutralBlack1,
+              ),
+            ],
+          ),
+          Text(
+            'Lawn Services',
+            style: TextStyle(
+              color: AppColors.subText,
+              fontSize: 12.sp,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 8.h),
+          Row(
+            children: [
+              const LittleTextWithColoredBG(
+                text: 'FCD10',
+                color: AppColors.blue,
+              ),
+              SizedBox(width: 4.w),
+              const LittleTextWithColoredBG(
+                text: 'Discount',
+                color: AppColors.yellow,
+              ),
+              SizedBox(width: 4.w),
+              Text(
+                '\$25.00',
+                style: TextStyle(
+                  color: AppColors.nutralBlack1,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
