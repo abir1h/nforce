0e88ea4f27f9239002cac158428499f30880f44c
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-11-18 14:49:38 +0600

    plugin section on service manager page

--- lib/app/modules/service_items/views/service_items_view.dart
+++ lib/app/modules/service_items/views/service_items_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:nuforce/app/modules/home/components/service_button.dart';
 import 'package:nuforce/app/modules/service_items/views/service_items_list_view.dart';
+import 'package:nuforce/app/modules/service_items/widgets/plugin_item_card.dart';
 import 'package:nuforce/app/shared/widgets/custom_appbar_minimal.dart';
 import 'package:nuforce/app/utils/colors.dart';
 import 'package:nuforce/gen/assets.gen.dart';
@@ -23,148 +24,190 @@ class ServiceItemsView extends GetView<ServiceItemsController> {
       body: SizedBox(
         height: height,
         width: width,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: width,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-                  child: Wrap(
-                    spacing: 16,
-                    runSpacing: 16,
-                    alignment: WrapAlignment.start,
-                    children: [
-                      ServiceButton(
-                        svgPath: Assets.images.svg.service,
-                        title: 'Service',
-                        onTap: () {
-                          late ServiceItemsController controller;
-                          if (!ServiceItemsController().initialized) {
-                            controller = Get.put(ServiceItemsController());
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: width,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                child: Wrap(
+                  spacing: 16,
+                  runSpacing: 16,
+                  alignment: WrapAlignment.start,
+                  children: [
+                    ServiceButton(
+                      svgPath: Assets.images.svg.service,
+                      title: 'Service',
+                      onTap: () {
+                        late ServiceItemsController controller;
+                        if (!ServiceItemsController().initialized) {
+                          controller = Get.put(ServiceItemsController());
+                        }
+                        controller.miniTabEnum.value = ServiceTabEnum.all;
+                        Get.to(() => const ServiceItemsListView());
+                      },
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.material,
+                      title: 'Material',
+                      onTap: () {
+                        late ServiceItemsController controller;
+                        if (!ServiceItemsController().initialized) {
+                          controller = Get.put(ServiceItemsController());
+                        }
+                        controller.miniTabEnum.value = ServiceTabEnum.material;
+                        Get.to(() => const ServiceItemsListView());
+                      },
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.package,
+                      title: 'Package',
+                      onTap: () {
+                        late ServiceItemsController controller;
+                        if (!ServiceItemsController().initialized) {
+                          controller = Get.put(ServiceItemsController());
+                        }
+                        controller.miniTabEnum.value = ServiceTabEnum.package;
+                        Get.to(() => const ServiceItemsListView());
+                      },
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.subscription,
+                      title: 'Subscription',
+                      onTap: () {
+                        late ServiceItemsController controller;
+                        if (!ServiceItemsController().initialized) {
+                          controller = Get.put(ServiceItemsController());
+                        }
+                        controller.miniTabEnum.value = ServiceTabEnum.subscription;
+                        Get.to(() => const ServiceItemsListView());
+                        Future.delayed(const Duration(milliseconds: 200)).then((value) {
+                          if (controller.scrollController.hasClients) {
+                            controller.scrollController.animateTo(
+                              controller.scrollController.position.maxScrollExtent,
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeInOut,
+                            );
                           }
-                          controller.miniTabEnum.value = ServiceTabEnum.all;
-                          Get.to(() => const ServiceItemsListView());
-                        },
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.material,
-                        title: 'Material',
-                        onTap: () {
-                          late ServiceItemsController controller;
-                          if (!ServiceItemsController().initialized) {
-                            controller = Get.put(ServiceItemsController());
+                        });
+                      },
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.addons,
+                      title: 'Addons',
+                      onTap: () {
+                        late ServiceItemsController controller;
+                        if (!ServiceItemsController().initialized) {
+                          controller = Get.put(ServiceItemsController());
+                        }
+                        controller.miniTabEnum.value = ServiceTabEnum.addons;
+                        Get.to(() => const ServiceItemsListView());
+                        Future.delayed(const Duration(milliseconds: 200)).then((value) {
+                          if (controller.scrollController.hasClients) {
+                            controller.scrollController.animateTo(
+                              controller.scrollController.position.maxScrollExtent,
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeInOut,
+                            );
                           }
-                          controller.miniTabEnum.value = ServiceTabEnum.material;
-                          Get.to(() => const ServiceItemsListView());
-                        },
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.package,
-                        title: 'Package',
-                        onTap: () {
-                          late ServiceItemsController controller;
-                          if (!ServiceItemsController().initialized) {
-                            controller = Get.put(ServiceItemsController());
-                          }
-                          controller.miniTabEnum.value = ServiceTabEnum.package;
-                          Get.to(() => const ServiceItemsListView());
-                        },
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.subscription,
-                        title: 'Subscription',
-                        onTap: () {
-                          late ServiceItemsController controller;
-                          if (!ServiceItemsController().initialized) {
-                            controller = Get.put(ServiceItemsController());
-                          }
-                          controller.miniTabEnum.value = ServiceTabEnum.subscription;
-                          Get.to(() => const ServiceItemsListView());
-                          Future.delayed(const Duration(milliseconds: 200)).then((value) {
-                            if (controller.scrollController.hasClients) {
-                              controller.scrollController.animateTo(
-                                controller.scrollController.position.maxScrollExtent,
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                              );
-                            }
-                          });
-                        },
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.addons,
-                        title: 'Addons',
-                        onTap: () {
-                          late ServiceItemsController controller;
-                          if (!ServiceItemsController().initialized) {
-                            controller = Get.put(ServiceItemsController());
-                          }
-                          controller.miniTabEnum.value = ServiceTabEnum.addons;
-                          Get.to(() => const ServiceItemsListView());
-                          Future.delayed(const Duration(milliseconds: 200)).then((value) {
-                            if (controller.scrollController.hasClients) {
-                              controller.scrollController.animateTo(
-                                controller.scrollController.position.maxScrollExtent,
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeInOut,
-                              );
-                            }
-                          });
-                        },
-                      ),
-                    ],
-                  ),
+                        });
+                      },
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(height: 24.sp),
-              Padding(
-                padding: EdgeInsets.only(left: 20.sp),
-                child: Text(
-                  'Promotions',
-                  style: TextStyle(
-                    color: AppColors.nutralBlack1,
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w600,
-                  ),
+            ),
+            SizedBox(height: 24.sp),
+            Padding(
+              padding: EdgeInsets.only(left: 20.sp),
+              child: Text(
+                'Promotions',
+                style: TextStyle(
+                  color: AppColors.nutralBlack1,
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              SizedBox(
-                width: width,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-                  child: Wrap(
-                    spacing: 16,
-                    runSpacing: 16,
-                    alignment: WrapAlignment.start,
-                    children: [
-                      ServiceButton(
-                        svgPath: Assets.images.svg.coupon,
-                        title: 'Coupon',
-                        onTap: () {},
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.discount,
-                        title: 'Discount',
-                        onTap: () {},
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.offer,
-                        title: 'Offer',
-                        onTap: () {},
-                      ),
-                      ServiceButton(
-                        svgPath: Assets.images.svg.gift,
-                        title: 'Gift',
-                        onTap: () {},
-                      ),
-                    ],
-                  ),
+            ),
+            SizedBox(
+              width: width,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                child: Wrap(
+                  spacing: 16,
+                  runSpacing: 16,
+                  alignment: WrapAlignment.start,
+                  children: [
+                    ServiceButton(
+                      svgPath: Assets.images.svg.coupon,
+                      title: 'Coupon',
+                      onTap: () {},
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.discount,
+                      title: 'Discount',
+                      onTap: () {},
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.offer,
+                      title: 'Offer',
+                      onTap: () {},
+                    ),
+                    ServiceButton(
+                      svgPath: Assets.images.svg.gift,
+                      title: 'Gift',
+                      onTap: () {},
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 24.sp),
+            Padding(
+              padding: EdgeInsets.only(left: 20.sp, right: 20.sp),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Plugins',
+                    style: TextStyle(
+                      color: AppColors.nutralBlack1,
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    'See All',
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                      color: AppColors.subText,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 16.h),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ListView.builder(
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: PluginItemCard(
+                        onTap: () {},
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/app/modules/service_items/widgets/plugin_item_card.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nuforce/app/utils/colors.dart';
+
+class PluginItemCard extends StatelessWidget {
+  const PluginItemCard({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(10.sp),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.sp),
+          border: Border.all(
+            color: AppColors.primaryBlue3,
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 90.sp,
+                  width: 90.sp,
+                  decoration: BoxDecoration(
+                    color: AppColors.primaryBlue3,
+                    borderRadius: BorderRadius.circular(4.sp),
+                  ),
+                ),
+                SizedBox(width: 12.sp),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'QuickBooks Customer Sync',
+                        style: TextStyle(
+                          color: AppColors.nutralBlack1,
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Sync your quick books customers with nuforce contacts',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: AppColors.nutralBlack1,
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '\$250.00',
+                            style: TextStyle(
+                              color: AppColors.nutralBlack1,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(width: 4.sp),
+                          Text(
+                            'One time fee',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: AppColors.nutralBlack1,
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 1.sp, horizontal: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: AppColors.green,
+                        ),
+                        child: Text(
+                          'Activated',
+                          style: TextStyle(
+                            color: AppColors.white1,
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
